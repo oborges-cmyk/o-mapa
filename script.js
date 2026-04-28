4486ad22043bcae633d25e70b7cf192ba625d4cc
isaraujou <o.borges@escola.pr.gov.br>
2026-04-28 08:48:35 -0300

    commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const avanca = document.querySelectorAll('.btn-próximo')
+
+avanca.forEach(buttton => { 
+    buttton.addEventListener('click', function() {
+        const atual = document.querySelector('.ativo');
+    })
+})
