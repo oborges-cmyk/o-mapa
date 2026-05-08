d32d1ba4ea68e075c38d7b979cae189271f64817
isaraujou <o.borges@escola.pr.gov.br>
2026-05-08 09:45:32 -0300

    a

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(buttton => { 
-    buttton.addEventListener('click', function(){
+    button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo');
         const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
         atual.classList.remove('ativo');
         document.getElementById(proximoPasso).classList.add('ativo');
     })
-})
-
-//document.getElementById("btnIr").addEventListener("click", function() {
-    //const destino = document.getElementById("sec2");
-   // if (destino) {
-       // window.scrollTo({
-         //   top: destino.offsetTop,
-        //    behavior: "smooth"
-        //});
-   // }
-//});
+}) 
