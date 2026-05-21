7190b4216d571e1abbeed6fbcbc0e31ddc2295b6
isaraujou <o.borges@escola.pr.gov.br>
2026-05-21 11:20:05 -0300

    a

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+//1. Selecionamos todos os botões de avanc=çar e voltar
+const botoes=document.querySelectorAll('.btn-proximo', '.btn-voltar');
+
+
+botoes.forEach(botao =>{ 
+    botao.addEventListener('click',() =>{
+        const proximoPassoId=botao.getAttribute('data-proximo') || botao.getAttribute('data-voltar');
+
+        trocarDePasso(proximoPassoId);
+ });
+});
+
+
 avanca.forEach(button =>{ 
     button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo');
